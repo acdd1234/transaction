05dade47a566b2750cc9ce4ab71839d1a2e14d53
amadou <amadoucd@gmail.com>
2019-08-22 15:04:17 -0400

    Connect DockerCompose and REST

--- src/main/java/com/acdd/transaction/TransactionApplication.java
+++ src/main/java/com/acdd/transaction/TransactionApplication.java
@@ -2,12 +2,23 @@ package com.acdd.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TransactionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionApplication.class, args);
 	}
+	
+	@RequestMapping(value="/")
+	public String hello(){
+		
+		return "Amadou";
+	}
+	
+	
 
 }
